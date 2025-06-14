7a0c1542ca935cb2dc79a8325f70010193e2a932
Siyanda Xulu <siyandampilwenhlexulu@gmail.com>
2025-06-14 23:54:45 +0200

    Lesson 3: Remove the 0 in the display

--- script.js
+++ script.js
@@ -8,8 +8,31 @@ let justCalculated = false; // Flag to track if a calculation was just performed
 function appendToDisplay(value) {
     console.log('Button pressed: ', value); 
 
-    alert('You pressed: ' + value);
+    let currentValue = display.value;
+
+    if (justCalculated && !isNaN(value)) { // If a calculation was just performed and the value is not a numbre than is will return true but because of the execution order(!) it will not append the value and will return false.
+        display.value = value;
+        justCalculated = false; // Reset the flag after appending a new value
+        return;
+    }
+
+    // If current display show 0 and user enters a number, we wanna replace the 0 with the new number
+    if (currentValue === '0' && !isNaN(value)) {
+        display.value = value;
+    // If the current display shows 0 and the user enters decimals, keep the 0
+    } else if (currentValue === '0' && value === '.') {
+        display.value = currentValue + value;
+    } else {
+        display.value = currentValue + value;
+    }
+
+    //Reset the justCalculated flag when user starts typing
+    justCalculated = false;
+
+    console.log('Display updated to: ', display.value);
 }
+
+   
 
 // Testing function to clear the display and show an alert
 function clearDisplay() {
@@ -22,6 +45,12 @@ function clearDisplay() {
 function deleteLast() {
     console.log('Backspace button pressed.');
 
+    // If theres only one character or its 0, reset to 0
+    if (currentValue.length <= 1 || currentValue === '0') {
+        display.value = '0';
+    } else {
+        display.value = currentValue.slice(0, -1); // Remove the last character
+    }
     alert('Backspace button was clicked');
 }
 
